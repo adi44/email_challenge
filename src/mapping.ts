03ff0ea8bd2ae83f9d855e17e50cec8ce94ce9d2
Aditya Dhir <31381639+adi44@users.noreply.github.com>
2022-01-02 22:14:46 +0530

    subgraph

---
+++ src/mapping.ts
@@ -0,0 +1,23 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { Email, Msg_Recieved } from "../generated/Email/Email"
+import { Message } from "../generated/schema"
+
+export function handleMsg_Recieved(event: Msg_Recieved): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let id = event.params.to.toHex() + event.params.id.toHex();
+  let message = Message.load(id)
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!message) {
+    message = new Message(id)
+
+    // Entity fields can be set using simple assignments
+    message.to = event.params.to.toHex();
+    message.from = event.params.from.toHex();
+    message.data = event.params.data.toHex();
+  }
+
+  message.save()
+}
