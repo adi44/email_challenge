4fda66f001b40f453b2caaadcfd9c88a8c7fd804
Aditya Dhir <31381639+adi44@users.noreply.github.com>
2022-01-06 19:35:13 +0530

    Removed variables and on chain Storage

--- Email.sol
+++ Email.sol
@@ -2,14 +2,8 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Email{
-    struct Message {
-        address to;
-        address from;
-        bytes data;
-    }
+
     uint256 public messageId = 0;
-    mapping (address =>mapping(uint=>Message) ) public messages;
-    address public owner;
     mapping (address => bool) public recievers;
 
     event Msg_Recieved(address to, address from, bytes data, uint256 id);
@@ -26,8 +20,6 @@ contract Email{
 
     function sendMessage(address to, bytes calldata data) public {
         require(recievers[to]==true,"Reciever is not registered");
-        Message memory message = Message(to,msg.sender,data);
-        messages[to][messageId]= message;
         messageId = messageId + 1;
         emit Msg_Recieved(to, msg.sender, data, messageId-1);
     }
