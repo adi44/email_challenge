ebf2bf2da868cc2f38030bd9e8dc6c7c24e3e085
Aditya Dhir <31381639+adi44@users.noreply.github.com>
2022-01-08 21:46:55 +0530

    updated the contract

--- Email.sol
+++ Email.sol
@@ -6,14 +6,13 @@ contract Email{
     //every email will have specific ID
     uint256 public messageId = 0;
     // mapping to for recievers to register
-    mapping (address => bool) public recievers;
+    mapping (address => bytes) public recievers;
 
     event Msg_Recieved(address to, address from, bytes data, uint256 id);
 
     // it will register a user who is calling this method
-    function register() public {
-        require(recievers[msg.sender]!=true,"Reciever already registed");
-        recievers[msg.sender] = true;
+    function register(bytes calldata publickey) public {
+        recievers[msg.sender] = publickey;
     }
 
     function sendMessage(address to, bytes calldata data) public {
